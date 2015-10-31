fbac702538d4eefb0a646df8f69da459044dee8a
iksaku <iksaku@me.com>
2015-10-30 18:35:47 -0600

    Added "TravisBuilder" script, added some things to core script

---
+++ TravisBuilder.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @name TravisBuilder
+ * @main TravisBuilder\Loader
+ * @version 1.0.0
+ * @api 1.12.0
+ * @description Makes PHAR files out of DevTools inside a Travis build
+ * @author iksaku
+ */
+ 
+namespace TravisBuilder{
+
+    use pocketmine\plugin\PluginBase;
+    use pocketmine\scheduler\PluginTask;
+
+    class Loader extends PluginBase{
+        public function onEnable(){
+            $plugin = array_diff($this->getServer()->getPluginManager()->getPlugins(), ["TravisBuilder"])[0];
+            if(count($plugin) < 1){
+                echo "[Error] No plugin found! Aborting...";
+                $this->getServer()->forceShutdown();
+            }else{
+                /** @var PluginBase $plugin */
+                $plugin = $this->getServer()->getPluginManager()->getPlugin($plugin);
+                $this->getServer()->getScheduler()->scheduleDelayedTask(new BuilderTask($this, $plugin), 20);
+            }
+        }
+
+        /**
+         * @param PluginBase $plugin
+         * @return string
+         */
+        public function pharName(PluginBase $plugin){
+            $name = getenv("BUILD_NAME");
+            if(!$name){
+                return $plugin->getName();
+            }
+            return $plugin->getName();
+        }
+    }
+
+    class BuilderTask extends PluginTask{
+        /** @var Loader */
+        private $core;
+        /** @var string */
+        public $plugin;
+
+        /**
+         * @param Loader $core
+         * @param PluginBase $plugin
+         */
+        public function __construct(Loader $core, PluginBase $plugin){
+            parent::__construct($plugin);
+            $this->core = $core;
+            $this->plugin = $plugin;
+        }
+
+        /**
+         * @return Loader
+         */
+        public function getPlugin(){
+            return $this->core;
+        }
+
+        public function onRun($currentTick){
+            $description = $this->plugin->getDescription();
+            $pluginPath = $this->getPlugin()->getDataFolder() . DIRECTORY_SEPARATOR . $this->getPlugin()->pharName($this->plugin) . ".phar";
+            $pharPath = getenv("PHAR_PATH");
+            if(!is_dir($pharPath)){
+                mkdir($pharPath);
+            }
+            $phar = new \Phar($pharPath);
+            $phar->setMetadata([
+                "name" => $description->getName(),
+                "version" => $description->getVersion(),
+                "main" => $description->getMain(),
+                "api" => $description->getCompatibleApis(),
+                "depend" => $description->getDepend(),
+                "description" => $description->getDescription(),
+                "authors" => $description->getAuthors(),
+                "website" => $description->getWebsite(),
+                "creationDate" => time()
+            ]);
+            $phar->setStub('<?php echo "PocketMine-MP plugin ' . $description->getName() . ' v' . $description->getVersion() . '\n----------------\n";if(extension_loaded("phar")){$phar = new \Phar(__FILE__);foreach($phar->getMetadata() as $key => $value){echo ucfirst($key).": ".(is_array($value) ? implode(", ", $value):$value)."\n";}}__HALT_COMPILER();');
+            $phar->setSignatureAlgorithm(\Phar::SHA1);
+            $phar->startBuffering();
+            foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($pluginPath)) as $file){
+                $path = ltrim(str_replace(["\\", $pluginPath], ["/", ""], $file), "/");
+                if(($path{0} === "." or strpos($path, "/.") !== false) or $file === "plugin.yml" or (strpos($path, "resources/") !== false or strpos($path, "src/"))){
+                    continue;
+                }
+                $phar->addFile($file, $path);
+                echo "[Info] Adding $path";
+            }
+            foreach($phar as $file => $finfo){
+                /** @var \PharFileInfo $finfo */
+                if($finfo->getSize() > (1024 * 512)){
+                    $finfo->compress(\Phar::GZ);
+                }
+            }
+            $phar->stopBuffering();
+            echo "[Info] PHAR file successfully created! Stopping server...";
+            $this->getPlugin()->getServer()->shutdown();
+            return true;
+        }
+    }
+}
--- script.php
+++ script.php
@@ -2,38 +2,50 @@
 $deployBranch = !getenv("DEPLOY_BRANCH") ? getenv("TRAVIS_BRANCH") : getenv("DEPLOY_BRANCH");
 $token = getenv("TOKEN");
 putenv("TOKEN=''");
-$buildName = getenv("BUILD_NAME");
 $pullRequest = getenv("TRAVIS_PULL_REQUEST") !== false;
+
+$rootPath = rtrim(getenv("TRAVIS_BUILD_DIR"), "/");
+$rootPath = explode("/", $rootPath);
+array_pop($rootPath);
+$rootPath = implode("/", $rootPath) . "/";
+
+$pharPath = $rootPath . "build";
+putenv("PHAR_PATH=$pharPath");
 if($pullRequest){
     echo "[Info] 'Pull Request' detected, build will not be deployed.";
 }else{
-    echo "[Info] '$deployBranch' is the target 'Deploy Branch'";
+    echo "[Info] '$deployBranch' is the target Deploy-Branch";
 }
 if(!$token){
     echo "[Warning] No 'GitHub Token' provided, build will not be deployed.";
 }
-echo "[Info] '" . (!$buildName ? "DevTools" : $buildName) . "' is the assigned 'Build Name' for the file.";
-echo "Setting up environment...";
-/*TODO:
- * Setup environment
- *  - Download and run PocketMine-MP
- *  - Install DevTools
- * Exit script if PocketMine-MP Crashes
- * ...
- */
+echo "[Info] Setting up environment...";
+chdir("$rootPath");
+exec("mkdir server");
+exec("mkdir build");
+chdir("server");
+exec("mkdir plugins");
+exec("cp " . getenv("TRAVIS_BUILD_DIR") . "/travis/TravisBuilder.php " . $rootPath . "server/plugins");
+exec("cp " . getenv("TRAVIS_BUILD_DIR") . " " . $rootPath . "server/plugins/" . array_pop(explode("/", getenv("TRAVIS_REPO_SLUG"))));
+exec("curl -sL get.pocketmine.net | bash -s - -v " . (getenv("PM_VERSION") !== false ? getenv("PM_VERSION") : "stable"));
+$pmLine = null;
+echo "[Info] Starting PocketMine-MP...";
+exec("./start.sh --diable-ansi --no-wizard", $t, $pmLine);
 
-if(!$pullRequest && $token !== false){
-    echo "Preparing to deploy...";
-    // Move into 'DevTools' directory...
+// TODO: Exit state of PMMP
+
+if(is_dir($pharPath) && !$pullRequest && $token !== false){
+    echo "[Info] Preparing to deploy...";
+    chdir("$pharPath");
     exec("git init");
-    exec("git remote add origin https://$TOKEN@github.com/" . getenv("TRAVIS_REPO_SLUG" . ".git"));
+    exec("git remote add origin https://$TOKEN@github.com/" . getenv("TRAVIS_REPO_SLUG"));
     exec("git fetch origin");
-    exec("git config user.name \"LegendsOfMCPE-Bot\"");
-    exec("git config user.email \"LoM_Bot@travis.ci\"");
-    exec("git add -A");
-    echo "Creating commit...";
+    exec("git config user.name \"iksaku's BuilderBot\"");
+    exec("git config user.email \"iksaku_Bot@travis.ci\"");
+    exec("git add .");
+    echo "[Info] Creating commit...";
     exec("git commit -m \"New Build! Revision: " . getenv("TRAVIS_COMMIT") . "\"");
-    echo "Pushing commit...";
+    echo "[Info] Pushing commit...";
     exec("git push --force --quiet origin HEAD:$deployBranch");
-    echo "Build successfully uploaded!";
+    echo "[Info] Build successfully uploaded!";
 }
