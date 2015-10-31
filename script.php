941201b5c69bbc2674b46e6506c66e0ca11f7409
iksaku <iksaku@me.com>
2015-10-30 18:52:12 -0600

    Changed 'exec("cd...")' to 'chdir("...")', use PocketMine's Travis-like processor for server start & finish

--- script.php
+++ script.php
@@ -3,13 +3,11 @@ $deployBranch = !getenv("DEPLOY_BRANCH") ? getenv("TRAVIS_BRANCH") : getenv("DEP
 $token = getenv("TOKEN");
 putenv("TOKEN=''");
 $pullRequest = getenv("TRAVIS_PULL_REQUEST") !== false;
-
 $rootPath = rtrim(getenv("TRAVIS_BUILD_DIR"), "/");
-$rootPath = explode("/", $rootPath);
-array_pop($rootPath);
-$rootPath = implode("/", $rootPath) . "/";
-
-$pharPath = $rootPath . "build";
+    $rootPath = explode("/", $rootPath);
+    array_pop($rootPath);
+    $rootPath = implode("/", $rootPath) . "/";
+    $pharPath = $rootPath . "build";
 putenv("PHAR_PATH=$pharPath");
 if($pullRequest){
     echo "[Info] 'Pull Request' detected, build will not be deployed.";
@@ -19,6 +17,7 @@ if($pullRequest){
 if(!$token){
     echo "[Warning] No 'GitHub Token' provided, build will not be deployed.";
 }
+
 echo "[Info] Setting up environment...";
 chdir("$rootPath");
 exec("mkdir server");
@@ -28,11 +27,21 @@ exec("mkdir plugins");
 exec("cp " . getenv("TRAVIS_BUILD_DIR") . "/travis/TravisBuilder.php " . $rootPath . "server/plugins");
 exec("cp " . getenv("TRAVIS_BUILD_DIR") . " " . $rootPath . "server/plugins/" . array_pop(explode("/", getenv("TRAVIS_REPO_SLUG"))));
 exec("curl -sL get.pocketmine.net | bash -s - -v " . (getenv("PM_VERSION") !== false ? getenv("PM_VERSION") : "stable"));
-$pmLine = null;
-echo "[Info] Starting PocketMine-MP...";
-exec("./start.sh --diable-ansi --no-wizard", $t, $pmLine);
 
-// TODO: Exit state of PMMP
+echo "[Info] Starting PocketMine-MP...";
+$server = proc_open(PHP_BINARY . "PocketMine-MP.phar --no-wizard --disable-readline", [
+    0 => ["pipe" => "w"],
+], $pipes);
+while(!feof($pipes[0])){
+    echo fgets($pipes[0]);
+}
+fclose($pipes[0]);
+echo "[Info] PocketMine-MP stopped: " . proc_close($server);
+if(count(glob($pharPath . "*.phar")) === 0){
+    echo "[Error] Plugin PHAR was not created!";
+    exit(1);
+}
+echo "[Info] Plugin PHAR successfully created!";
 
 if(is_dir($pharPath) && !$pullRequest && $token !== false){
     echo "[Info] Preparing to deploy...";
@@ -49,3 +58,5 @@ if(is_dir($pharPath) && !$pullRequest && $token !== false){
     exec("git push --force --quiet origin HEAD:$deployBranch");
     echo "[Info] Build successfully uploaded!";
 }
+
+exit(0);
