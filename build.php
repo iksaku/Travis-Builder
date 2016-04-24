9ced5898930115475d5873b402e54cb8e6927e41
Jorge González <iksaku@me.com>
2016-04-24 18:54:40 -0500

    Testing...

--- build.php
+++ build.php
@@ -12,14 +12,22 @@ function info(string $message, int $prefix = 0){
     echo("\n[" . $tag[$prefix] . "] " . $message);
 }
 
-function do_command(string $command): bool{
-    exec($command, $output, $status);
-    return $status < 1;
+function do_command($command): bool{
+    if(!is_array($command)){
+        $command = [$command];
+    }
+    foreach($command as $cmd){
+        exec($cmd, $output, $code);
+        if($code > 0){
+            return false;
+        }
+    }
+    return true;
 }
 
-function get_base(string $string): string{
+function get_base(string $string, bool $last = true): string{
     $parts = explode("/", $string);
-    return $parts[1];
+    return $last ? $parts[count($parts) + 1] : $parts[0];
 }
 
 function validEnv(string $var){
@@ -33,7 +41,6 @@ if(getenv("TRAVIS_PULL_REQUEST") !== "false"){
     info("Pull Request detected! Quitting...");
     exit(0);
 }
-
 
 $repo = validEnv("DEPLOY_REPO") ?? getenv("TRAVIS_REPO_SLUG");
 $branch = validEnv("DEPLOY_REPO") ?? "travis-build";
@@ -64,9 +71,11 @@ info("Preparing Build environment...");
 @mkdir("build");
 # Move files to build
 $files = ["resources", "src", "LICENSE", "plugin.yml", "README.md"];
-foreach($files as $f){
+foreach($files as $k => $f){
     if(is_dir($f) or file_exists($f)){
         do_command("mv $f build/$f");
+    }else{
+        unset($files[$k]);
     }
 }
 # Download DevTools to build the PHAR
@@ -84,12 +93,9 @@ info("PHAR successfully built!");
 if($token !== false){
     info("Deploying...");
     foreach($files as $f){
-        if(is_dir($f) or file_exists($f)){
-            do_command("mv build/$f $f");
-        }
+        do_command("mv build/$f $f");
     }
-    $git = [
-        #"mv " . $build_name . " ../" . $build_name,
+    if(!do_command([
         "git remote set-url origin https://" . $token . "@github.com/" . $repo . ".git",
         "git fetch --all",
         "git pull --all",
@@ -97,18 +103,27 @@ if($token !== false){
         "git config user.email \"iksaku@me.com\"",
         "git config push.default simple",
         "git checkout -b " . $branch,
-        "rm -rf * ($build_name)",
-        "git rm -rf *",
-        #"mv ../" . $build_name . " " . $build_name,
-        "git add -A",
+    ])){
+        info("Something went wrong while configuring Git Repo", 2);
+        exit(1);
+    }
+    exec("git ls-files", $output);
+    foreach($output as $f){
+        if(is_file($f) and strpos($f, "/") !== false){
+            $f = explode("/", $f)[0];
+        }
+        if($f !== ".gitignore" and $f !== "test.php"){
+            exec("git rm -rf " . $f);
+        }
+    }
+    exec("git ls-files", $output);
+    if(!do_command([
+        "git add " . $build_name,
         "git commit -m \"(" . getenv("TRAVIS_BUILD_NUMBER") . ") New Build! Revision: " . getenv("TRAVIS_COMMIT") . "\"",
         "git push",
-    ];
-    foreach($git as $cmd){
-        if(!do_command($cmd)){
-            info("Something went wrong while deploying. Is your Token/Information still valid?", 2);
-            exit(1);
-        }
+    ])){
+        info("Something went wrong while deploying. Is your Token/Information still valid?", 2);
+        exit(1);
     }
 
     info("Successfully Deployed build. Enjoy!");
