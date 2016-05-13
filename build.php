1a40177a67d70c32f5544a561f8eb45bcf928821
Jorge González <iksaku@me.com>
2016-05-13 12:21:46 -0500

    Deprecated Git Deployment, Bintray for the win!

--- build.php
+++ build.php
@@ -7,7 +7,7 @@ if(PHP_VERSION_ID < 70000){
 function info(string $message, int $prefix = 0){
     $tag = ["Info", "Warning", "Error"];
     if(!isset($tag[$prefix])){
-        $prefix = 1;
+        $prefix = 0;
     }
     echo("\n[" . $tag[$prefix] . "] " . $message);
 }
@@ -42,33 +42,27 @@ if(getenv("TRAVIS_PULL_REQUEST") !== "false"){
     exit(0);
 }
 
-$repo = validEnv("DEPLOY_REPO") ?? getenv("TRAVIS_REPO_SLUG");
-$branch = validEnv("DEPLOY_REPO") ?? "travis-build";
-$token = validEnv("DEPLOY_TOKEN") ?? false;
-
 # Mess with Build tags
+$pluginInfo = yaml_parse_file("plugin.yml");
 $name_tags = [
-    "@number" => "TRAVIS_BUILD_NUMBER",
-    "@commit" => "TRAVIS_COMMIT"
+    "api" => $pluginInfo["api"],
+    "commit" => validEnv("TRAVIS_COMMIT"),
+    "number" => validEnv("TRAVIS_BUILD_NUMBER"),
+    "version" => $pluginInfo["version"]
 ];
-$build_name = get_base(validEnv("BUILD_NAME") ?? $repo);
+$build_name = get_base(validEnv("BUILD_NAME") ?? validEnv("DEPLOY_REPO"));
 foreach($name_tags as $k => $v){
-    if(!empty(getenv($v))){
-        str_replace($k, $v, $build_name);
-    }
+    str_replace("%" . $k . "%", $v, $build_name);
 }
 if(substr($build_name, -5, 5) !== ".phar"){
     $build_name .= ".phar";
 }
 
 # Get back to workflow...
-if(!$token){
-    info("No \"Token\" provided, \"Build\" will not be deployed", 1);
-}else{
-    info("Build will deploy to repo: " . $repo . ", branch: " . $branch . ". Unless token is invalid...");
-}
 info("Preparing Build environment...");
-@mkdir("build");
+if(!is_dir("build")){
+    mkdir("build");
+}
 # Move files to build
 $files = ["resources", "src", "LICENSE", "plugin.yml", "README.md"];
 foreach($files as $k => $f){
@@ -79,54 +73,15 @@ foreach($files as $k => $f){
     }
 }
 # Download DevTools to build the PHAR
+info("Downloading DevTools...");
 if(!do_command("curl -sL https://github.com/PocketMine/DevTools/releases/download/v1.11.0/DevTools_v1.11.0.phar -o DevTools.phar")){
-    info("Couldn't download DevTools. We sorry...", 2);
+    info("Couldn't download DevTools. Sorry...", 2);
     exit(1);
 }
 # Build...
+info("Building PHAR...");
 if(!do_command("php -dphar.readonly=0 DevTools.phar --make build --out " . $build_name) && !file_exists($build_name)){
     info("Something went wrong while Building. Sorry! :(", 2);
     exit(1);
 }
 info("PHAR successfully built!");
-
-if($token !== false){
-    info("Deploying...");
-    foreach($files as $f){
-        do_command("mv build/$f $f");
-    }
-    if(!do_command([
-        "git remote set-url origin https://" . $token . "@github.com/" . $repo . ".git",
-        "git fetch --all",
-        "git pull --all",
-        "git config user.name \"TravisBuilder (By @iksaku)\"",
-        "git config user.email \"iksaku@me.com\"",
-        "git config push.default simple",
-    ])){
-        info("Something went wrong while configuring Git Repo", 2);
-        exit(1);
-    }
-    if(!do_command("git checkout --orphan " . $branch)){
-        do_command("git checkout -B " . $branch);
-    }
-    exec("git ls-files", $output);
-    foreach($output as $f){
-        if(is_file($f) and strpos($f, "/") !== false){
-            $f = get_base($f, false);
-        }
-        if($f !== ".gitignore" and $f !== "test.php"){
-            exec("git rm -rf " . $f);
-        }
-    }
-    exec("git ls-files", $output);
-    if(!do_command([
-        "git add " . $build_name,
-        "git commit -m \"(" . getenv("TRAVIS_BUILD_NUMBER") . ") New Build! Revision: " . getenv("TRAVIS_COMMIT") . "\"",
-        "git push --force --quiet --set-upstream origin " . $branch,
-    ])){
-        info("Something went wrong while deploying. Is your Token/Information still valid?", 2);
-        exit(1);
-    }
-
-    info("Successfully Deployed build. Enjoy!");
-}
